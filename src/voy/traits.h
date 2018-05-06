5b3d824d0656a31684422a79bbc2d0015fce4892
Ivan Čukić <ivan.cukic@kde.org>
2018-05-06 22:28:51 +0200

    Initial commit to start off the reactive streams development

---
+++ src/voy/traits.h
@@ -0,0 +1,125 @@
+/*
+ *   Copyright (C) 2018 Ivan Čukić <ivan.cukic(at)kde.org>
+ *
+ *   This library is free software; you can redistribute it and/or
+ *   modify it under the terms of the GNU Lesser General Public
+ *   License as published by the Free Software Foundation; either
+ *   version 2.1 of the License, or (at your option) version 3, or any
+ *   later version accepted by the membership of KDE e.V. (or its
+ *   successor approved by the membership of KDE e.V.), which shall
+ *   act as a proxy defined in Section 6 of version 3 of the license.
+ *
+ *   This library is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *   Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public
+ *   License along with this library.
+ *   If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef VOY_TRAITS_H
+#define VOY_TRAITS_H
+
+// STL
+#include <type_traits>
+
+// Some meta-programming utility functions and meta-functions,
+// including the detection idiom - the poor man's concepts
+
+#define voy_concept inline constexpr bool
+#define voy_require(Condition) typename std::enable_if_t<Condition, int> = 0
+
+namespace voy::traits {
+
+// Meta-function to force the compiler to print out the exact type passed
+// to it by creating a compilation error because the type is not implemented
+template <typename... Types>
+struct print_types;
+
+// Meta-function that always returns false. It is useful for marking a
+// particular if-constexpr branch to be erroneous
+template <typename T>
+voy_concept always_fail = false;
+
+#define voy_fail(Type, Msh) static_assert(voy::traits::always_fail<Type>, Msg>
+
+
+// C++20 Stuff
+
+// Meta-function which strips out the reference and cv qualifiers from the type
+template <typename T>
+using remove_cvref_t = std::remove_reference_t<std::remove_cv_t<T>>;
+
+
+// The detection idiom -- static introspection made easy
+
+struct nonesuch {
+    nonesuch() = delete;
+    ~nonesuch() = delete;
+    nonesuch(const nonesuch&) = delete;
+    nonesuch& operator=(const nonesuch&) = delete;
+};
+
+namespace detail {
+    template < typename Default
+             , typename Void
+             , template <typename...> typename Operation
+             , typename... Args
+             >
+    struct detector {
+        using value_t = std::false_type;
+        using type = Default;
+    };
+
+    template < typename Default
+             , template <typename...> typename Operation
+             , typename... Args
+             >
+    struct detector < Default
+                    , std::void_t<Operation<Args...>>
+                    , Operation
+                    , Args...
+                    > {
+        using value_t = std::true_type;
+        using type = Operation<Args...>;
+    };
+} // namespace detail
+
+
+template < template <typename...> typename Operation
+         , typename... Args
+         >
+using is_detected = typename detail::detector < nonesuch
+                                               , void
+                                               , Operation
+                                               , Args...
+                                               >::value_t;
+
+
+template < template <typename...> typename Operation
+         , typename... Args
+         >
+voy_concept is_detected_v = is_detected<Operation, Args...>::value;
+
+
+template < template <typename...> typename Operation
+         , typename... Args
+         >
+using is_detected_t = typename is_detected<Operation, Args...>::type;
+
+
+template < template <typename...> typename Operation
+         , typename... Args
+         >
+using detected_t = typename detail::detector < nonesuch
+                                             , void
+                                             , Operation
+                                             , Args...>::type;
+
+
+} // namespace voy::traits
+
+#endif // include guard
+
---
+++ src/voy/utils.h
@@ -0,0 +1,101 @@
+/*
+ *   Copyright (C) 2018 Ivan Čukić <ivan.cukic(at)kde.org>
+ *
+ *   This library is free software; you can redistribute it and/or
+ *   modify it under the terms of the GNU Lesser General Public
+ *   License as published by the Free Software Foundation; either
+ *   version 2.1 of the License, or (at your option) version 3, or any
+ *   later version accepted by the membership of KDE e.V. (or its
+ *   successor approved by the membership of KDE e.V.), which shall
+ *   act as a proxy defined in Section 6 of version 3 of the license.
+ *
+ *   This library is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *   Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public
+ *   License along with this library.
+ *   If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef VOY_UTILS_H
+#define VOY_UTILS_H
+
+#include <utility>
+
+// This file contains some things that would be cool to have in
+// the standard library. There might be a chance some of these will
+// end up in C++20. And a few additional utility functions and macros.
+
+#define voy_fwd(Var) std::forward<decltype(Var)>(Var)
+#define voy_fwd_invoke(Fn, Var) std::invoke(Fn, voy_fwd(Var))
+
+namespace voy::utils {
+
+// Private copy constructor and copy assignment ensure classes derived
+// from class noncopyable cannot be copied.
+namespace disable_adl_ {
+    class non_copyable {
+    protected:
+        constexpr non_copyable() = default;
+        ~non_copyable() = default;
+
+    private:
+        non_copyable(const non_copyable&) = delete;
+        non_copyable& operator=(const non_copyable&) = delete;
+    };
+}
+
+using non_copyable = disable_adl_::non_copyable;
+
+
+// Function object that takes a value, and does nothing,
+// really useful
+
+inline auto do_nothing = [] (auto&& value)
+{
+};
+
+
+// Identity function
+
+inline auto identity = [] (auto&& value)
+{
+    return voy_forward(value);
+};
+
+
+// Overloaded lambdas
+
+template <typename... Fs>
+struct overloaded: Fs... {
+    using Fs::operator()...;
+};
+
+template <typename... Fs>
+overloaded(Fs...) -> overloaded<Fs...>;
+
+
+// Execute code when exiting scope
+
+template <typename F>
+struct on_scope_exit {
+    on_scope_exit(F f)
+        : m_deinit{std::move(f)}
+    {
+    }
+
+    ~on_scope_exit()
+    {
+        m_deinit();
+    }
+
+    F m_deinit;
+};
+
+
+} // namespace voy::utils
+
+#endif // include guard
+
